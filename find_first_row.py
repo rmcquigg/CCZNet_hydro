7f3df7e5aac61e711b38134bdd9d0b6f8daf11d4
Rachel McQuiggan <55991096+rmcquigg@users.noreply.github.com>
2021-01-09 12:29:36 -0500

    Update find_first_row.py

--- find_first_row.py
+++ find_first_row.py
@@ -4,6 +4,8 @@ Created on Sat Jan  9 09:12:34 2021
 
 @author: mcquiggan
 """
+
+#Finds the number of rows to skip from a Solinst datalogger .csv file
 import csv
 def first_row(filename):
     with open(filename,encoding='gbk') as f:
