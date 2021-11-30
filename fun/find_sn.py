d224addcc9a805317aff292765db30bd79bf26e8
Rachel McQuiggan <55991096+rmcquigg@users.noreply.github.com>
2021-11-30 14:47:34 -0500

    Update and rename find_sn.py to fun/find_sn.py

--- fun/find_sn.py
+++ fun/find_sn.py
@@ -4,7 +4,7 @@ Created on Tue Aug 24 12:23:29 2021
 
 @author: mcquiggan
 """
-
+#Get datalogger serial number from csv download file
 import csv
 def find_sn(path,file):
     with open(path+file) as f:
